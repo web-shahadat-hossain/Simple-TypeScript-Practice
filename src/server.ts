9f5e092fe0c913e083991048452512de4775da0e
web-shahadat-hossain <mdridoysikder862@gmail.com>
2023-05-07 00:50:47 +0600

    TypeScript tuple

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+// TypeScript tuple
+
+const userInfo: [number, string, string, boolean, string] = [
+  101,
+  "Ema",
+  "John",
+  true,
+  "2023",
+];
+
+console.log(userInfo);
