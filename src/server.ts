1e656de846a266994f910579015b62ebb28d5431
web-shahadat-hossain <mdridoysikder862@gmail.com>
2023-05-07 01:14:08 +0600

    CommonElements

--- src/server.ts
+++ src/server.ts
@@ -8,4 +8,23 @@ const userInfo: [number, string, string, boolean, string] = [
   "2023",
 ];
 
-console.log(userInfo);
+// console.log(userInfo);
+
+// Common Elements in array
+const getCommonElements = (
+  numberArray1: number[],
+  numberArray2: number[]
+): number[] => {
+  const commonElements: number[] = [];
+
+  for (let num1 of numberArray1) {
+    // console.log(num1);
+    if (numberArray2.includes(num1)) {
+      commonElements.push(num1);
+    }
+  }
+
+  return commonElements;
+};
+
+console.log(getCommonElements([1, 4], [1, 5]));
