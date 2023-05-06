6057da305c21736e525290572e833f16c8b1dda6
web-shahadat-hossain <mdridoysikder862@gmail.com>
2023-05-07 02:02:20 +0600

    You want to filter an array of Products based on a specific criterion and value.

--- src/server.ts
+++ src/server.ts
@@ -27,4 +27,51 @@ const getCommonElements = (
   return commonElements;
 };
 
-console.log(getCommonElements([1, 4], [1, 5]));
+// console.log(getCommonElements([1, 4], [1, 5]));
+
+// You have an interface for Product, containing the product's id, name, price, and category. You want to filter an array of Products based on a specific criterion and value.
+
+interface IProduct {
+  name: string;
+  id: number;
+  price: number;
+  category: string;
+}
+
+const product = <T, U extends keyof T>(
+  productInfo: T[],
+  criterion: U,
+  value: T[U]
+): T[] => {
+  return productInfo.filter((info) => info[criterion] === value);
+};
+
+const products: IProduct[] = [
+  {
+    name: "Ti Shirt",
+    id: 1,
+    price: 23,
+    category: "Shirt",
+  },
+  {
+    name: "Ti Shirt",
+    id: 2,
+    price: 10,
+    category: "Shirt",
+  },
+  {
+    name: "panjabi",
+    id: 3,
+    price: 23,
+    category: "panjabi",
+  },
+  {
+    name: "new panjabi",
+    id: 4,
+    price: 25,
+    category: "panjabi",
+  },
+];
+
+const result = product(products, "price", 23);
+console.log(result);
